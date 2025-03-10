c5a408158b2815dc986b965b3efc66148df6d6a7
Vanessa Teixeira <vanessa.r.m.teixeira@gmail.com>
2025-03-10 11:52:07 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Vanessa Teixeira and is {""}
+        <a
+          href="https://github.com/vanessateixeira92/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
